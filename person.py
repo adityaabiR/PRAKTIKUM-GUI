3ef2d2e43b3d8ce63e6487b9f11a64b1944bcf73
adityaabiR <72436170+adityaabiR@users.noreply.github.com>
2021-06-11 02:36:20 +0700

    Add files via upload

--- person.py
+++ person.py
@@ -1,7 +1,8 @@
 class Person:
-  def __init__(self, fname, lname):
+  def __init__(self, fname,mname, lname):
     self.firstname = fname
+    self.middlename = mname
     self.lastname = lname
 
   def printname(self):
-    print(self.firstname, self.lastname)
+    print(self.firstname, self.middlename, self.lastname)
